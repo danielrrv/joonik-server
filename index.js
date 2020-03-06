3278b8b298482a1dfa575f573be7a61d8ad58593
danielrrv <drodrigo678@gmail.com>
2020-03-06 00:24:29 -0500

    shopifyTo

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/shopify', (req, res) => {
 
     shopifyToken.shop = APP_SHOP.replace('.myshopify', '')
     const nonce = shopifyToken.generateNonce();
-    const uri = shopifyToken.generateAuthUrl(shopifyToken.shop,['read_products','write_products'], nonce);
+    const uri = shopifyToken.generateAuthUrl(shopifyToken.shop, ['read_products', 'write_products'], nonce);
     // const redirectUri = forwardingAddress + '/shopify/callback';
     // const installUrl = 'https://' + APP_SHOP +
     //     '/admin/oauth/authorize?client_id=' + apiKey +
@@ -66,64 +66,102 @@ app.get('/shopify/callback', (req, res) => {
         return res.status(403).send('Request origin cannot be verified');
     }
 
-    if (shop && hmac && code) {
-        // DONE: Validate request is from Shopify
-        const map = Object.assign({}, req.query);
-        delete map['signature'];
-        delete map['hmac'];
-        const message = querystring.stringify(map);
-        const providedHmac = Buffer.from(hmac, 'utf-8');
-        const generatedHash = Buffer.from(
-            crypto
-                .createHmac('sha256', apiSecret)
-                .update(message)
-                .digest('hex'),
-            'utf-8'
-        );
-        let hashEquals = false;
 
-        try {
-            hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
-        } catch (e) {
-            hashEquals = false;
-        };
+    if (!shop || !hmac || !code) {
+        return res.status(422).json({ status: "Unsopported Action" });
+    }
 
-        if (!hashEquals) {
-            return res.status(400).send('HMAC validation failed');
+    let token
+    if (shopifyToken.verifyHmac(request.query)) {
+        // Get permanent access token that will be used in the future to make API calls
+        const data = await shopifyToken.getAccessToken(shop, code)
+        let timeNow = + new Date()
+        const expiresAt = timeNow + (data.expires_in - 20 * 1000)
+        const tokenData = { ...data, expires_at: expiresAt } // TODO: change from unix timestamp to Firestore date
+        token = tokenData.access_token
+    } else {
+
+        return response.status(500).json({ status: 'Error occurred', error: e.stack })
+    }
+    if (token) {
+
+        const shopRequestUrl = 'https://' + shop + '/admin/api/2020-01/shop.json';
+        const shopRequestHeaders = {
+            'X-Shopify-Access-Token': token,
         }
-
-        // DONE: Exchange temporary code for a permanent access token
-        const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
-        const accessTokenPayload = {
-            client_id: apiKey,
-            client_secret: apiSecret,
-            code,
-        };
-
-        request.post(accessTokenRequestUrl, { json: accessTokenPayload })
-            .then((accessTokenResponse) => {
-                const accessToken = accessTokenResponse.access_token;
-                // DONE: Use access token to make API call to 'shop' endpoint
-                const shopRequestUrl = 'https://' + shop + '/admin/api/2020-01/shop.json';
-                const shopRequestHeaders = {
-                    'X-Shopify-Access-Token': accessToken,
-                };
-
-                request.get(shopRequestUrl, { headers: shopRequestHeaders })
-                    .then((shopResponse) => {
-                        res.status(200).end(shopResponse);
-                    })
-                    .catch((error) => {
-                        res.status(error.statusCode).send(error.error.error_description);
-                    });
+        request.get(shopRequestUrl, { headers: shopRequestHeaders })
+            .then((shopResponse) => {
+                res.status(200).end(shopResponse);
             })
             .catch((error) => {
                 res.status(error.statusCode).send(error.error.error_description);
             });
 
-    } else {
-        res.status(400).send('Required parameters missing');
+
     }
+
+
+
+
+    // if (shop && hmac && code) {
+    //     // DONE: Validate request is from Shopify
+    //     const map = Object.assign({}, req.query);
+    //     delete map['signature'];
+    //     delete map['hmac'];
+    //     const message = querystring.stringify(map);
+    //     const providedHmac = Buffer.from(hmac, 'utf-8');
+    //     const generatedHash = Buffer.from(
+    //         crypto
+    //             .createHmac('sha256', apiSecret)
+    //             .update(message)
+    //             .digest('hex'),
+    //         'utf-8'
+    //     );
+    //     let hashEquals = false;
+
+    //     try {
+    //         hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+    //     } catch (e) {
+    //         hashEquals = false;
+    //     };
+
+    //     if (!hashEquals) {
+    //         return res.status(400).send('HMAC validation failed');
+    //     }
+
+    // DONE: Exchange temporary code for a permanent access token
+
+    // const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+    // const accessTokenPayload = {
+    //     client_id: apiKey,
+    //     client_secret: apiSecret,
+    //     code,
+    // };
+
+    // request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+    //     .then((accessTokenResponse) => {
+    //         const accessToken = accessTokenResponse.access_token;
+    //         // DONE: Use access token to make API call to 'shop' endpoint
+    //         const shopRequestUrl = 'https://' + shop + '/admin/api/2020-01/shop.json';
+    //         const shopRequestHeaders = {
+    //             'X-Shopify-Access-Token': accessToken,
+    //         };
+
+    //         request.get(shopRequestUrl, { headers: shopRequestHeaders })
+    //             .then((shopResponse) => {
+    //                 res.status(200).end(shopResponse);
+    //             })
+    //             .catch((error) => {
+    //                 res.status(error.statusCode).send(error.error.error_description);
+    //             });
+    //     })
+    //     .catch((error) => {
+    //         res.status(error.statusCode).send(error.error.error_description);
+    //     });
+
+    // } else {
+    //     res.status(400).send('Required parameters missing');
+    // }
 });
 
 
