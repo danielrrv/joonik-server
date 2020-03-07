838a6b75e6c966f7f0e4564169d6f81437096a33
danielrrv <drodrigo678@gmail.com>
2020-03-07 01:08:20 -0500

    ui

--- lib/components/app.jsx
+++ lib/components/app.jsx
@@ -1,68 +1,15 @@
 import React from "react";
+import { AppProvider } from '@shopify/polaris';
+import translations from '@shopify/polaris/locales/en.json';
+import Login from './Login'
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.handleButtonClick = this.handleButtonClick.bind(this);
-        this.handleTextChange = this.handleTextChange.bind(this);
-        this.handleReset = this.handleReset.bind(this);
-
-        this.state = {
-            name: "",
-            msg: ""
-        };
-    }
-
-    //Handlers
-    handleButtonClick = e => {
-        const nameLen = this.state.name.length;
-        if (nameLen > 0) {
-            this.setState({
-                msg: `You name has ${nameLen} characters including space`
-            });
-        }
-    };
-
-    handleTextChange = e => {
-        this.setState({ name: e.target.value });
-    };
-
-    handleReset = () => {
-        this.setState({ name: "", msg: "" });
-    };
-    //End Handlers
-
-    render() {
-        let msg;
-
-        if (this.state.msg !== "") {
-            msg = <p>{this.state.msg}</p>;
-        } else {
-            msg = "";
-        }
-        return (
-            //do something here where there is a button that will replace the text
-            <div>
-                {/* <button onClick={()=>window.location.href='/'}>Ir a commercio</button> */}
-                <label>Your name </label>
-                <input
-                    type="text"
-                    id="txtName"
-                    name="txtName"
-                    value={this.state.name}
-                    onChange={this.handleTextChange}
-                />
-                
-                <button id="btnSubmit" onClick={this.handleButtonClick}>
-                    Calculate Name Length
-        </button>
-                <button id="btnReset" onClick={this.handleReset}>
-                    Reset All
-        </button>
-                <hr />
-                {msg}
-            </div>
-        );
-    }
-}
+const App = () => {
+    // const { Component, pageProps } = props;
+    return (
+        <AppProvider i18n={translations}>
+            <Login />
+            {/* <Component {...pageProps} /> */}
+        </AppProvider>
+    );
+};
 export default App;
