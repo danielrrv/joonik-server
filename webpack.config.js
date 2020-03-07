a706ccccc3ac7c8369912e353c651a6aa141d701
danielrrv <drodrigo678@gmail.com>
2020-03-07 16:17:02 -0500

    sdsd

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 // const webpack = require('webpack')
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const config = {
   mode: "production",
