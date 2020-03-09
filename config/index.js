70b0f01686f2ad7f1f955f23ffc511b0630f742f
danielrrv <drodrigo678@gmail.com>
2020-03-09 00:21:03 -0500

    asdasd

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const SHOPIFY_APP_URL = 'joonikshop.myshopify.com';//public
 const EXTERNAL_APP_URL = 'https://joonik-node.herokuapp.com'
-const SCOPES = 'read_products'
+const SCOPES = ['read_products', 'write_products']
 const EMAIL = 'user@example.com';
 const PASSWORD = '123456';
 
