f497afd312c2b33428aa4eaaaced4f7b7ca0d939
danielrrv <drodrigo678@gmail.com>
2020-03-09 14:50:05 -0500

    asdasd

--- lib/components/app.jsx
+++ lib/components/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppProvider } from '@shopify/polaris';
+import { AppProvider, TypeOf } from '@shopify/polaris';
 import translations from '@shopify/polaris/locales/en.json';
 import Main from './Main'
 import { Provider } from '@shopify/app-bridge-react';
@@ -13,14 +13,25 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 
 
 
-const client = new ApolloClient({
-    // fetchOptions:{
-    //     credentials:'include'
-    // }
-    ssrMode:true,
-    link:createHttpLink({ uri: "/graphql", fetch }),
-    cache: new InMemoryCache()
-});
+let client;
+if (typeof window === 'undefined') {
+    client = new ApolloClient({
+        // fetchOptions:{
+        //     credentials:'include'
+        // }
+        ssrMode: true,
+        link: createHttpLink({ uri: "/graphql", fetch }),
+        cache: new InMemoryCache()
+    });
+}
+else {
+    client = new ApolloClient({
+        fetchOptions: {
+            credentials: 'include'
+        }
+    }
+
+}
 
 const App = () => {
     const config = { apiKey: process.env.SHOPIFY_API_KEY, shopOrigin: SHOPIFY_APP_URL, forceRedirect: true }
