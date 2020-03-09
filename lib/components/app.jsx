aebb5e0f1fca5341fe0e4cf9b46e3239126df45b
danielrrv <drodrigo678@gmail.com>
2020-03-09 15:39:49 -0500

    asdasd

--- lib/components/app.jsx
+++ lib/components/app.jsx
@@ -7,29 +7,27 @@ import { SHOPIFY_APP_URL } from '../../config/index'
 // import ApolloClient from 'apollo-boost';
 import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
-// import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 
 
-let client;
-if (typeof window === 'undefined') {
-    client = new ApolloClient({
-        // fetchOptions:{
-        //     credentials:'include'
-        // }
-        ssrForceFetchDelay: 100,
-        link: createHttpLink({ uri: "/graphql"}),
-        cache: new InMemoryCache().restore(window.__APOLLO_STATE__)
-    });
-} else {
-    client = new ApolloClient({
-        fetchOptions: {
-            credentials: 'include'
-        }
-    });
-}
+// let client;
+// if (typeof window === 'undefined') {
+//     client = new ApolloClient({
+//         // fetchOptions:{
+//         //     credentials:'include'
+//         // }
+//         ssrForceFetchDelay: 100,
+//         link: createHttpLink({ uri: "/graphql", fetch}),
+//     });
+// } else {
+const client = new ApolloClient({
+    link:createHttpLink({ uri: "/graphql", fetch}),
+    cache:new InMemoryCache()
+});
+// }
 
 const App = () => {
     const config = { apiKey: process.env.SHOPIFY_API_KEY, shopOrigin: SHOPIFY_APP_URL, forceRedirect: true }
